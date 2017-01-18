e3c75eb190490cee4b89173aded0de5dbc6608b3
Kaleb Wasmuth <kalebwasmuth@gmail.com>
2017-01-18 00:40:11 -0500

    Added perms for bouncyjeffer.

--- traineeBot.js
+++ traineeBot.js
@@ -69,6 +69,9 @@ function Comment(text, sender, dateSent, isHidden, commentID){
 
 //    <><><> permissionNodes <><><>
 var users = {
+    bouncyjeffer: {
+	permissionNode: 'regMod'  
+    },
     brandonvalencia: {
         permissionNode: 'regMod'
     },
